950942aa78a1ea6ed9d7bb447bbca06dd53a795d
ierostenko <hitmax87@mail.ru>
2016-07-19 10:58:41 +0300

    create plane + limited size stack

---
+++ LimitedSizeStack.cs
@@ -0,0 +1,35 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+
+public class LimitedSizeStack<T> : LinkedList<T>
+{
+    private readonly int _maxSize;
+    public LimitedSizeStack(int maxSize)
+    {
+        _maxSize = maxSize;
+    }
+
+    public void Push(T item)
+    {
+        this.AddFirst(item);
+
+        if (this.Count > _maxSize)
+            this.RemoveLast();
+    }
+
+    public T Pop()
+    {
+        var item = this.First.Value;
+        this.RemoveFirst();
+        return item;
+    }
+
+    public T Peek()
+    {
+        if (this.Count == 0)
+            return default(T);
+
+        return this.First.Value;
+    }
+}
